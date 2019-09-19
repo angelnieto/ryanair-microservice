cc6978c4029924c682874b25e6a26982d26ddda5
PASTRANA\rmc <rmolina@comunytek.com>
2019-09-19 08:31:42 +0200

    Added @Configuration

--- src/main/java/es/rmc/config/SpringConfig.java
+++ src/main/java/es/rmc/config/SpringConfig.java
@@ -1,8 +1,10 @@
 package es.rmc.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration
 public class SpringConfig {
 
 	@Bean
